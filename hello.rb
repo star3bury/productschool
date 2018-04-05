954862cbffa21dd3ffe54abbc7d04c582805153a
Nelson Yu <nelson.yu@gmail.com>
2018-04-05 03:06:41 +0000

    hello.rb moved into repo

---
+++ hello.rb
@@ -0,0 +1,10 @@
+puts "Hello world!"
+
+gift = [
+          "next year's xmas tree",
+          "a golf pencil",
+          "a slice of stale wedding cake",
+          "a medium-sized island off the coast of Sumatra",
+          "a visual prosthesis",
+         ]
+  puts "All I got for valentine's day was #{gift.sample}."
